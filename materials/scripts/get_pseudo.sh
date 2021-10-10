dd627df27558648af9100ffd435f160ee0144569
Davide Donadio <ddonadio@ucdavis.edu>
2021-10-09 22:37:00 -0700

    Add files via upload

--- materials/scripts/get_pseudo.sh
+++ materials/scripts/get_pseudo.sh
@@ -6,7 +6,6 @@ TUTORIALPWD=/home/donadio/Tutorials/Denton_School
 wget https://www.quantum-espresso.org/upf_files/Ga.pbe-dnl-kjpaw_psl.1.0.0.UPF
 wget https://www.quantum-espresso.org/upf_files/As.pbe-n-kjpaw_psl.1.0.0.UPF
 wget https://www.quantum-espresso.org/upf_files/Al.pbe-nl-kjpaw_psl.1.0.0.UPF
-wget https://www.quantum-espresso.org/upf_files/Mo.pbesol-spn-kjpaw_psl.1.0.0.UPF
-wget https://www.quantum-espresso.org/upf_files/S.pbe-n-kjpaw_psl.1.0.0.UPF
+wget https://www.quantum-espresso.org/upf_files/H.pbe-kjpaw_psl.1.0.0.UPF
 
 mv *UPF $TUTORIALPWD/Pseudo
